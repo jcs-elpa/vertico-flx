cfa25674a45560c672e06635fa006dcb30043fbe
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-19 17:15:08 +0800

    src

---
+++ vertico-flx.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: vertico-flx.el $
+;; $Date: 2022-04-19 17:13:20 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2022 by Shen, Jen-Chieh $
+;; ========================================================================
