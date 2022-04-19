70273dd306cb35c92650624e75a948c410a233ed
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-19 21:14:31 +0800

    Bump min version

--- vertico-flx.el
+++ vertico-flx.el
@@ -7,7 +7,7 @@
 ;; Description: Flx integration for vertico.
 ;; Keyword: vertico flx
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1") (vertico "0.22") (flx "0.5") (flx-style "0.1.1") (ht "2.0") (f "0.20.0") (mbs "0.1.0"))
+;; Package-Requires: ((emacs "27.1") (vertico "0.22") (flx "0.5") (flx-style "0.1.1") (ht "2.0") (f "0.20.0") (mbs "0.1.0"))
 ;; URL: https://github.com/jcs-elpa/vertico-flx
 
 ;; This file is NOT part of GNU Emacs.
